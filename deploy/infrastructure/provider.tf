6fd14e42069f05beb5eb08f3d5d63567f778f0c6
Felix Chapman <aelred717@gmail.com>
2022-01-10 22:26:26 +0000

    remove required_providers

--- deploy/infrastructure/provider.tf
+++ deploy/infrastructure/provider.tf
@@ -1,16 +1,3 @@
-terraform {
-  required_providers {
-    aws = {
-      source = "hashicorp/aws"
-      version = "~> 3.28.0"
-    }
-    github = {
-      source = "integrations/github"
-      version = "~> 4.4.0"
-    }
-  }
-}
-
 provider aws {
   region = "eu-west-2"
 }
